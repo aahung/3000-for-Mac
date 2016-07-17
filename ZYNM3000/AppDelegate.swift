89f31d9c43154103e44059d1d09cd68ef77cf699
aaHung <Landxh@gmail.com>
2016-07-17 22:37:12 +0800

    Initial Commit

---
+++ ZYNM3000/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ZYNM3000
+//
+//  Created by Xinhong LIU on 17/7/2016.
+//  Copyright © 2016 XH. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ZYNM3000/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ZYNM3000
+//
+//  Created by Xinhong LIU on 17/7/2016.
+//  Copyright © 2016 XH. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
